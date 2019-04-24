14d99c51af20ee8aca81034bebe16cfeaf1636ee
chiuy5 <chiuy5@uw.edu>
2019-04-24 11:55:25 -0700

    Updates

---
+++ precache-manifest.c37df7bf97b1ea6779650b006882f897.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a508e4f02b22a948e9",
+    "url": "/drifting-test/drifting-app/build/static/css/main.8b111f89.chunk.css"
+  },
+  {
+    "revision": "12a508e4f02b22a948e9",
+    "url": "/drifting-test/drifting-app/build/static/js/main.b59bd35f.chunk.js"
+  },
+  {
+    "revision": "c2eb7c17731988884384",
+    "url": "/drifting-test/drifting-app/build/static/js/runtime~main.76937cee.js"
+  },
+  {
+    "revision": "ce324cfc6a0c44f54beb",
+    "url": "/drifting-test/drifting-app/build/static/css/2.17ccdb34.chunk.css"
+  },
+  {
+    "revision": "ce324cfc6a0c44f54beb",
+    "url": "/drifting-test/drifting-app/build/static/js/2.34795658.chunk.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/drifting-test/drifting-app/build/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "155f0f70e8ea0bd7e55ec7e73848cab0",
+    "url": "/drifting-test/drifting-app/build/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/drifting-test/drifting-app/build/precache-manifest.c37df7bf97b1ea6779650b006882f897.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/drifting-test/drifting-app/build/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
