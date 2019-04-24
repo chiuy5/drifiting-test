6ee65e0d97ed7048f85b2da748fd6819ebe7d600
chiuy5 <chiuy5@uw.edu>
2019-04-24 11:27:53 -0700

    made changes

--- drifting-app/src/App.js
+++ drifting-app/src/App.js
@@ -11,64 +11,16 @@ import Forum from "./Forum";
 import Home from "./Home";
 
 class App extends Component {
-  state = {
-    fields: {}
-  };
-
-  onSubmit = fields => {
-    this.setState({ fields });
-  };
 
   render() {
     return (
-      <div className="App">
-
-        {/* We might use nav bar, I am just saving the code for later */}
-
-        {/* <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#Forum">Forum</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Form inline>
-              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Navbar> */
-
-        /*<header className="App-header">
-          <Image src="drifting-logo.jpg" className="drifting-logo" />
-        </header> */
-
-        /* Trying to create a container and make a new page */
-
-        /*<Container className="forumSubmission">
-          <ForumSubmission onSubmit={fields => this.onSubmit(fields)} />
-        </Container> */
-
-        /*<Container className="Encouragement">
-          <Encouragement onSubmit={fields => this.onSubmit(fields)} />
-        </Container>*/}
-
         <Router>
-          <div>
-            <Route exact path="/" render={()=> <Home />} />
-            <Route path="/processing" render={() => <ForumSubmission onSubmit={fields => this.onSubmit(fields)} />} />
-            <Route path="/encourage" render={() => <Encouragement onSubmit={fields => this.onSubmit(fields)} />} />
+          <div className="App">
+            <Route exact path="/" render={() => <Home />} />
+            <Route path="/processing" render={() => <ForumSubmission />} />
+            <Route path="/encourage" render={() => <Encouragement />} />
           </div>
         </Router>
-
-
-      </div>
     );
   }
 }
